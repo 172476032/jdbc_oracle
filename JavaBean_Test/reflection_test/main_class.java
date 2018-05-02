0c5d918ce4f00a1f3c2cc523d722d4b0ed2e8625
172476032 <172476032@qq.com>
2018-05-02 11:43:24 +0800

    class_test

---
+++ JavaBean_Test/reflection_test/main_class.java
@@ -0,0 +1,11 @@
+package reflection_test;
+
+public class main_class {
+public main_class() {
+	// TODO Auto-generated constructor stub
+	getclass_test classType = new getclass_test();
+	System.out.println("-------------");
+
+	System.out.println(classType.getClass()+","+ classType.getClass().getName());
+}
+}
